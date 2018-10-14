e5f454f356207604a52097b6d1c307c8f26cfec4
HannahChege <chegehannah45@gmail.com>
2018-10-14 14:51:11 +0300

    IMported the rest framework in the view function

--- awards/views.py
+++ awards/views.py
@@ -7,7 +7,7 @@
 # from .models import Project,Profile
 # from django.contrib.auth.models import User
 # from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework.views import APIView
 from .models import  MoringaMerch
 from .serializer import MerchSerializer
 # # Create your views here.
