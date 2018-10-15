168552fc203259a313684ee1d3f1dad193adb495
HannahChege <chegehannah45@gmail.com>
2018-10-15 16:37:30 +0300

    awards/views.py

--- awards/models.py
+++ awards/models.py
@@ -88,12 +88,61 @@ class Project(models.Model):
        return profile
 
 
+class DesignRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10')
+    )
+    project = models.ForeignKey(Project)
+    pub_date = models.DateTimeField(auto_now=True)
+    profile = models.ForeignKey(Profile)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=0)
 
-class Votes(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    project =  models.ForeignKey(Project,on_delete=models.CASCADE,related_name='reviews')
-    design = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
-    usability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
-    creativity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
-    content = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
 
+class UsabilityRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10')
+    )
+    project = models.ForeignKey(Project)
+    pub_date = models.DateTimeField(auto_now=True)
+    profile = models.ForeignKey(Profile)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=0)
+
+
+class ContentRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10')
+    )
+    project = models.ForeignKey(Project)
+    pub_date = models.DateTimeField(auto_now=True)
+    profile = models.ForeignKey(Profile)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=0)
