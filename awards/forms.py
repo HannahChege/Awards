3b5f95d7b5f428298e44721a891185ad6d659c44
HannahChege <chegehannah45@gmail.com>
2018-10-15 17:05:28 +0300

    awards/forms.py

--- awards/forms.py
+++ awards/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile,Project,Votes
+from .models import Profile,Project,ContentRating,DesignRating,UsabilityRating
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
