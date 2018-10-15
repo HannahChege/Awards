35909f572e1db62c856608d4e149ca04f9a45098
HannahChege <chegehannah45@gmail.com>
2018-10-15 17:06:00 +0300

    awards/forms.py

--- awards/serializer.py
+++ awards/serializer.py
@@ -4,9 +4,9 @@ from .models import Profile,Project
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id',bio', 'dp', 'user')
+        fields = ('id','bio','dp','user')
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id',title', 'description', 'image')        
+        fields = ('id','title', 'description', 'image')        
