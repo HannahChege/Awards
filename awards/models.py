262843da92a46f80b981b556f167eb9b8af940d3
HannahChege <chegehannah45@gmail.com>
2018-10-20 06:04:56 +0300

    Added the awards models

--- awards/models.py
+++ awards/models.py
@@ -19,6 +19,40 @@ def save_profile(sender, instance,**kwargs):
    instance.profile.save()
 # Create your models here.
 
+class Profile(models.Model):
+    """
+    Class that contains Profile details
+    """
+    dp = models.ImageField(upload_to = 'images/', blank=True)
+    bio = models.TextField()
+    contact = models.CharField(max_length = 30, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    post_save.connect(save_user_profile, sender=User)
+
+    def save_profile(self):
+        self.save()
+
+    def del_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+            profile = Profile.objects.get(user_id = user_id)
+            return profile
+
+    def __str__(self):
+                return self.bio
+
 class Project(models.Model):
     title = models.TextField()
     image = models.ImageField(upload_to = 'images/', default='No image')
